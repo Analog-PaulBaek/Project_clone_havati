52c97cd8dc1006c460a48f83a92992b96d4ed984
workk94 <workk94@gmail.com>
2024-07-26 06:16:13 +0900

    add swiper popup

---
+++ module.js
@@ -0,0 +1,23 @@
+export const itemsList = [
+  {
+    productName: '[SWEATER] 스웨터 (WHITE)',
+    size: ['X', 'M', 'L'],
+    productPrice: 50000,
+    imgUrl: 'https://via.placeholder.com/200X200',
+    cart_qty: 0,
+  },
+  {
+    productName: '[PANTS] 바지 (BLUE)',
+    size: ['X', 'M', 'L'],
+    productPrice: 100000,
+    imgUrl: 'https://via.placeholder.com/200X200',
+    cart_qty: 0,
+  },
+  {
+    productName: '[SHIRTER] 셔츠 (BLACK)',
+    size: ['X', 'M', 'L'],
+    productPrice: 150000,
+    imgUrl: 'https://via.placeholder.com/200X200',
+    cart_qty: 0,
+  },
+];
