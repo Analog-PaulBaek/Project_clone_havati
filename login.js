83fdb35e6866b21c57afc951688e16fe1c71e02a
workk94 <workk94@gmail.com>
2024-07-28 18:27:04 +0900

    로그인 유효성 확인 추가

---
+++ login.js
@@ -0,0 +1,90 @@
+// // 서브메뉴 검색 언더라인
+
+// const searchInput = document.querySelector("#search_input");
+// const searchBtn = document.querySelector("#search_btn");
+
+// searchBtn.addEventListener("click", function(){
+//     searchInput.classList.toggle("active");
+//     searchInput.focus();
+// })
+
+// // 사이드메뉴
+
+// const sideMenuBtn = document.querySelector("#sidemenu_btn");
+// const sideMenu = document.querySelector("#side_menu")
+// const sideMenuShow = document.getElementById("sidemenu_show")
+
+// sideMenuBtn.addEventListener("click", function(){
+//     sideMenu.classList.add('show');
+//     sideMenuShow.classList.add('back');
+// })
+
+// sideMenuShow.addEventListener('click', function(){
+//     sideMenu.classList.remove('show');
+//     sideMenuShow.classList.remove('back');
+// })
+
+// // 스크롤 상단 메뉴 크기 조정
+// const menu = document.querySelector('.header-fix');
+// const menuHeight = menu.getBoundingClientRect().height;
+// const headerWrap = document.querySelector('.header-wrap');
+// const mainLogo = document.querySelector(".main-logo");
+// const mainMenu = document.querySelector(".main-menu");
+
+// document.addEventListener('scroll', () => {
+//   if (window.scrollY > menuHeight) {
+//     headerWrap.classList.add('scrolldown');
+//     mainLogo.classList.add('logoscrolldown');
+//     mainMenu.classList.add('menuscrolldown');
+
+//   } else {
+//     headerWrap.classList.remove('scrolldown');
+//     mainLogo.classList.remove('logoscrolldown');
+//     mainMenu.classList.remove('menuscrolldown');
+//   }
+// });
+
+// // 상단 스크롤 버튼
+// const btnTop = document.querySelector(".btn_top");
+
+// document.addEventListener('scroll', () => {
+// 	if (window.scrollY > 20){
+// 		showBtn();
+// 	} else{
+// 		hideBtn();
+// 	}
+// });
+
+// function showBtn() {
+//   btnTop.classList.add("showbtntop");
+// }
+
+// function hideBtn() {
+//   btnTop.classList.remove("showbtntop");
+// }
+
+// btnTop.click(function(){
+// 	$('html, body').animate({scrollTop:0},400);
+// 	return false;
+// });
+
+// // 로그인 부분
+
+// 로그인 버튼
+function check() {
+  let frm = document.frm;
+  let member_id = frm.member_id;
+  let member_passwd = frm.member_passwd;
+
+  if (member_id.value == '') {
+    alert('아이디를 입력하세요');
+    member_id.focus();
+    return;
+  } else if (member_passwd.value == '') {
+    alert('비밀번호를 입력하세요');
+    member_passwd.focus();
+    return;
+  } else {
+    alert('로그인 되었습니다!');
+  }
+}
