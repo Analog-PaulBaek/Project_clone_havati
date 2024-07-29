3b51a57fbe43c7fb15f4087f223006f22bc74714
workk94 <workk219@gmail.com>
2024-07-29 14:03:17 +0900

    login.html pressEnter 추가, popup 오늘 하루 보지않기 추가

--- login.js
+++ login.js
@@ -1,77 +1,4 @@
-// // 서브메뉴 검색 언더라인
-
-// const searchInput = document.querySelector("#search_input");
-// const searchBtn = document.querySelector("#search_btn");
-
-// searchBtn.addEventListener("click", function(){
-//     searchInput.classList.toggle("active");
-//     searchInput.focus();
-// })
-
-// // 사이드메뉴
-
-// const sideMenuBtn = document.querySelector("#sidemenu_btn");
-// const sideMenu = document.querySelector("#side_menu")
-// const sideMenuShow = document.getElementById("sidemenu_show")
-
-// sideMenuBtn.addEventListener("click", function(){
-//     sideMenu.classList.add('show');
-//     sideMenuShow.classList.add('back');
-// })
-
-// sideMenuShow.addEventListener('click', function(){
-//     sideMenu.classList.remove('show');
-//     sideMenuShow.classList.remove('back');
-// })
-
-// // 스크롤 상단 메뉴 크기 조정
-// const menu = document.querySelector('.header-fix');
-// const menuHeight = menu.getBoundingClientRect().height;
-// const headerWrap = document.querySelector('.header-wrap');
-// const mainLogo = document.querySelector(".main-logo");
-// const mainMenu = document.querySelector(".main-menu");
-
-// document.addEventListener('scroll', () => {
-//   if (window.scrollY > menuHeight) {
-//     headerWrap.classList.add('scrolldown');
-//     mainLogo.classList.add('logoscrolldown');
-//     mainMenu.classList.add('menuscrolldown');
-
-//   } else {
-//     headerWrap.classList.remove('scrolldown');
-//     mainLogo.classList.remove('logoscrolldown');
-//     mainMenu.classList.remove('menuscrolldown');
-//   }
-// });
-
-// // 상단 스크롤 버튼
-// const btnTop = document.querySelector(".btn_top");
-
-// document.addEventListener('scroll', () => {
-// 	if (window.scrollY > 20){
-// 		showBtn();
-// 	} else{
-// 		hideBtn();
-// 	}
-// });
-
-// function showBtn() {
-//   btnTop.classList.add("showbtntop");
-// }
-
-// function hideBtn() {
-//   btnTop.classList.remove("showbtntop");
-// }
-
-// btnTop.click(function(){
-// 	$('html, body').animate({scrollTop:0},400);
-// 	return false;
-// });
-
-// // 로그인 부분
-
-// 로그인 버튼
-function check() {
+function login() {
   let frm = document.frm;
   let member_id = frm.member_id;
   let member_passwd = frm.member_passwd;
@@ -88,3 +15,17 @@ function check() {
     alert('로그인 되었습니다!');
   }
 }
+
+function pressEnter(event) {
+  if (event.key === 'Enter') {
+    login();
+  }
+}
+
+const loginBtn = document.querySelector('.login_btn');
+const userId = document.querySelector('#user_id');
+const userPw = document.querySelector('#user_pw');
+
+loginBtn.addEventListener('click', login);
+userId.addEventListener('keyup', pressEnter);
+userPw.addEventListener('keyup', pressEnter);
