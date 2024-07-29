61a803d173f686a0f3e0e49d07cec9cc23c72946
workk94 <workk219@gmail.com>
2024-07-29 18:07:45 +0900

    signUp.html 추가, module.js 수정, 링크 수정

--- item.js
+++ item.js
@@ -1,44 +1,41 @@
 // 서브메뉴 검색 언더라인
 
-const searchInput = document.querySelector("#search_input");
-const searchBtn = document.querySelector("#search_btn");
+const searchInput = document.querySelector('#search_input');
+const searchBtn = document.querySelector('#search_btn');
 
-
-searchBtn.addEventListener("click", function () {
-  searchInput.classList.toggle("active");
+searchBtn.addEventListener('click', function () {
+  searchInput.classList.toggle('active');
   searchInput.focus();
-})
+});
 
-// 사이드메뉴 
+// 사이드메뉴
 
-const sideMenuBtn = document.querySelector("#sidemenu_btn");
-const sideMenu = document.querySelector("#side_menu")
-const sideMenuShow = document.getElementById("sidemenu_show")
+const sideMenuBtn = document.querySelector('#sidemenu_btn');
+const sideMenu = document.querySelector('#side_menu');
+const sideMenuShow = document.getElementById('sidemenu_show');
 
-sideMenuBtn.addEventListener("click", function () {
+sideMenuBtn.addEventListener('click', function () {
   sideMenu.classList.add('show');
   sideMenuShow.classList.add('back');
-})
+});
 
 sideMenuShow.addEventListener('click', function () {
   sideMenu.classList.remove('show');
   sideMenuShow.classList.remove('back');
-})
-
+});
 
 // 스크롤 상단 메뉴 크기 조정
 const menu = document.querySelector('.header-fix');
 const menuHeight = menu.getBoundingClientRect().height;
 const headerWrap = document.querySelector('.header-wrap');
-const mainLogo = document.querySelector(".main-logo");
-const mainMenu = document.querySelector(".main-menu");
+const mainLogo = document.querySelector('.main-logo');
+const mainMenu = document.querySelector('.main-menu');
 
 document.addEventListener('scroll', () => {
   if (window.scrollY > menuHeight) {
     headerWrap.classList.add('scrolldown');
     mainLogo.classList.add('logoscrolldown');
     mainMenu.classList.add('menuscrolldown');
-
   } else {
     headerWrap.classList.remove('scrolldown');
     mainLogo.classList.remove('logoscrolldown');
@@ -46,10 +43,8 @@ document.addEventListener('scroll', () => {
   }
 });
 
-
 // 상단 스크롤 버튼
-const btnTop = document.querySelector(".btn_top");
-
+const btnTop = document.querySelector('.btn_top');
 
 document.addEventListener('scroll', () => {
   if (window.scrollY > 20) {
@@ -60,18 +55,17 @@ document.addEventListener('scroll', () => {
 });
 
 function showBtn() {
-  btnTop.classList.add("showbtntop");
+  btnTop.classList.add('showbtntop');
 }
 
 function hideBtn() {
-  btnTop.classList.remove("showbtntop");
+  btnTop.classList.remove('showbtntop');
 }
 
 btnTop.click(function () {
   $('html, body').animate({ scrollTop: 0 }, 400);
   return false;
 });
-
 
 function change(e) {
   let bigImg = document.querySelector('.big_img > img');
@@ -84,14 +78,10 @@ function change(e) {
 
 let frm = document.frm;
 let parent = document.getElementById('parent');
-let price = document.getElementById("itemprice");
+let price = document.getElementById('itemprice');
 let itemName = document.getElementById('itemname').innerHTML; // 상품 이름.
 
-
-
-
 function addList() {
-
   let selectOption = frm.selectbox.options[selectbox.selectedIndex];
 
   if (frm.selectbox.selectedIndex === 0) {
@@ -101,98 +91,91 @@ function addList() {
   let size = selectOption.text;
   console.log(selectOption);
 
-  let tr = document.createElement("tr"); // 부모
+  let tr = document.createElement('tr'); // 부모
 
   let td1 = document.createElement('td'); // 1. td 요소 생성
-  td1.innerHTML = "<strong>" + itemName + "</strong> - " + size;
+  td1.innerHTML = '<strong>' + itemName + '</strong> - ' + size;
   // 상품 이름 끝
 
   let td2 = document.createElement('td'); // td 생성
 
   let divAmount = document.createElement('div'); // div 생성
-  divAmount.id = "amount";
+  divAmount.id = 'amount';
 
   let input = document.createElement('input');
-  input.setAttribute("type", "text");
-  input.setAttribute("value", "1");
+  input.setAttribute('type', 'text');
+  input.setAttribute('value', '1');
 
   let amountBtn = document.createElement('div');
-  amountBtn.id = "amount-btn";
+  amountBtn.id = 'amount-btn';
 
   let amountUp = document.createElement('img');
-  amountUp.setAttribute("src", "https://img.echosting.cafe24.com/design/skin/default/product/btn_count_up.gif");
+  amountUp.setAttribute(
+    'src',
+    'https://img.echosting.cafe24.com/design/skin/default/product/btn_count_up.gif'
+  );
 
   let br = document.createElement('br');
 
   let amountDown = document.createElement('img');
-  amountDown.setAttribute("src", "https://img.echosting.cafe24.com/design/skin/default/product/btn_count_down.gif");
+  amountDown.setAttribute(
+    'src',
+    'https://img.echosting.cafe24.com/design/skin/default/product/btn_count_down.gif'
+  );
 
   amountBtn.append(amountUp, br, amountDown);
 
   let deleteBtn = document.createElement('img');
-  deleteBtn.id = "delete-btn";
-  deleteBtn.setAttribute("src", "https://img.echosting.cafe24.com/design/skin/default/product/btn_price_delete.gif");
-
+  deleteBtn.id = 'delete-btn';
+  deleteBtn.setAttribute(
+    'src',
+    'https://img.echosting.cafe24.com/design/skin/default/product/btn_price_delete.gif'
+  );
 
   divAmount.append(input, amountBtn, deleteBtn);
 
-
-  td2.append(divAmount)
-
+  td2.append(divAmount);
 
   let td3 = document.createElement('td');
-  td3.id = "td3";
-  td3.class = "cartprice";
-  td3.innerHTML = price.innerHTML + "원";
+  td3.id = 'td3';
+  td3.class = 'cartprice';
+  td3.innerHTML = price.innerHTML + '원';
 
   tr.append(td1, td2, td3);
 
-  let insertObj = document.getElementById("insert_obj");
+  let insertObj = document.getElementById('insert_obj');
   insertObj.parentNode.insertBefore(tr, insertObj);
 
-  amountUp.addEventListener("click", () => {
-
-    input.value = parseInt(input.value) + 1
+  amountUp.addEventListener('click', () => {
+    input.value = parseInt(input.value) + 1;
     td3.innerHTML = parseInt(td3.innerHTML);
-    td3.innerHTML = price.innerHTML * input.value + "원";
+    td3.innerHTML = price.innerHTML * input.value + '원';
     totalPrice();
   });
 
-  amountDown.addEventListener("click", () => {
+  amountDown.addEventListener('click', () => {
     input.value = parseInt(input.value) - 1;
     td3.innerHTML = parseInt(td3.innerHTML);
-    td3.innerHTML = price.innerHTML * input.value + "원";
+    td3.innerHTML = price.innerHTML * input.value + '원';
     totalPrice();
   });
-
 
   let remove = () => {
     tr.remove();
     totalPrice();
-  }
-  deleteBtn.addEventListener("click", remove);
+  };
+  deleteBtn.addEventListener('click', remove);
 
   function totalPrice() {
     let sum = document.getElementById('sum');
     total = 0;
-    let priceInts = document.querySelectorAll("#td3");
-    priceInts.forEach(priceInt => {
-    total += parseInt(priceInt.textContent);
+    let priceInts = document.querySelectorAll('#td3');
+    priceInts.forEach((priceInt) => {
+      total += parseInt(priceInt.textContent);
     });
     console.log(total);
     sum.textContent = total;
   }
 
   totalPrice();
-
 }
-
-
-
-
-
-
-
-
-
-
